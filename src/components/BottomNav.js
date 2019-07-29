8a49d274f06b8b157e191c7ebb056d9bfd967c66
Diamond Alexander <diamondnalexander@gmail.com>
2019-07-29 11:34:29 -0600

    added main nav icons and styling

---
+++ src/components/BottomNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BottomNav.css';
+
+const BottomNav = () => (
+  <div className="nav-container">
+    <div className="icons-container">
+      <div className="icon"></div>
+      <div className="icon"></div>
+      <div className="icon"></div>
+    </div>
+  </div>
+)
+
+export default BottomNav;
