1adeba515e256978fcf1743fa5b495941110365c
Diamond Alexander <diamondnalexander@gmail.com>
2019-07-29 19:02:28 -0600

    testing out color grid

--- src/pages/color.js
+++ src/pages/color.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import BottomNav from '../components/BottomNav';
+// import BottomNav from '../components/BottomNav';
 import BlobShape from '../components/BlobShape';
+import ColorGrid from '../components/ColorGrid';
+
+function showGrid() {
+  var grid = document.getElementById('grid');
+  grid.classList.toggle('hidden');
+}
+
+const BottomNav = (props) => (
+  <div className="nav-container">
+    <div className="icons-container" id={props.id}>
+      <div className="icon">
+        <a href={props.icon1link}>
+          {props.icon1}
+          <p>{props.icon1label}</p>
+        </a>
+      </div>
+      <div className="icon" onClick={showGrid}>
+          {props.icon2}
+          <p>{props.icon2label}</p>
+      </div>
+      <div className="icon">
+        <a href={props.icon3link}>
+          {props.icon3}
+          <p>{props.icon3label}</p>
+        </a>
+      </div>
+    </div>
+  </div>
+)
 
 const Color = () => (
   <div className="app">
@@ -8,7 +37,18 @@ const Color = () => (
       shapestyle={{filter: 'url(#f1)'}}
       fill="#BDBDBD"
     />
-    <BottomNav
+
+    <BottomNav 
+      id="ci-controls" 
+      icon1= {<img src={require('../images/shape.svg')} alt="circle icon for shape options" />}
+      icon2= {<img src={require('../images/color.svg')} alt="color drop icon for color options" />}
+      icon3= {<img src={require('../images/sticker.svg')} alt="sticker icon for sticker options"/>}
+      icon1link="/shape"
+      icon3link="/sticker"
+    />
+
+
+    {/* <BottomNav
       id="ci-controls" 
       icon1= {<img src={require('../images/shape.svg')} alt="circle icon for shape options" />}
       icon2= {<img src={require('../images/color.svg')} alt="color drop icon for color options" />}
@@ -16,7 +56,11 @@ const Color = () => (
       icon1link="/shape"
       icon2link="/color"
       icon3link="/sticker"
-    />
+    /> */}
+
+    <div id="grid" className="hidden">
+      <ColorGrid />
+    </div>
   </div>
 )
 
