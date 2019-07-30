2de326a0211586de252becfbdd4ac34ad1da55fe
Diamond Alexander <diamondnalexander@gmail.com>
2019-07-29 19:05:12 -0600

    uncommented code in main navigation component

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,34 +1,26 @@
 import React from 'react';
 import './BottomNav.css';
 
-function showGrid() {
-  var grid = document.getElementById('grid');
-  grid.classList.toggle('hidden');
-  var nav = document.getElementById('nav');
-  nav.classList.add('hidden');
-
-}
-
 const BottomNav = (props) => (
   <div className="nav-container">
     <div className="icons-container" id={props.id}>
       <div className="icon" onClick={props.link1}>
-        
+        <a href={props.icon3link}>
           {props.icon1}
           <p>{props.icon1label}</p>
-        
+        </a>
       </div>
       <div className="icon" onClick={props.link2}>
-        {/* <a href={props.icon2link}> */}
+        <a href={props.icon2link}>
           {props.icon2}
           <p>{props.icon2label}</p>
-        {/* </a> */}
+        </a>
       </div>
       <div className="icon" onClick={props.link3}>
-        {/* <a href={props.icon3link}> */}
+        <a href={props.icon3link}>
           {props.icon3}
           <p>{props.icon3label}</p>
-        {/* </a> */}
+        </a>
       </div>
     </div>
   </div>
