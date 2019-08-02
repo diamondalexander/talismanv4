1b5bcf14396ebab28864befb9e418a2cf9d61bb0
Diamond Alexander <diamondnalexander@gmail.com>
2019-08-01 20:44:14 -0600

    added emotion stickers

--- src/components/StickerGrid.js
+++ src/components/StickerGrid.js
@@ -7,20 +7,37 @@ function close(){
 }
 
 const StickerGrid = () => (
-
-  <div className="container">
-   <div><p id="close" onClick={close}>X Close</p></div>
+<div className="container">
+    <div><p id="close" onClick={close}>X Close</p></div>
     <div id="grid-row">
-      <button id="swatch1" className="pots" style={{backgroundColor: '#EFE2AB'}}></button>
-      <button id="swatch2" className="pots" style={{backgroundColor: '#E27B67'}}></button>
-      <button id="swatch3" className="pots" style={{backgroundColor: '#915249'}}></button>
-      <button id="swatch4" className="pots" style={{backgroundColor: '#6E4870'}}></button>
-      <button id="swatch5" className="pots" style={{backgroundColor: '#B785BA'}}></button>
-      <button id="swatch6" className="pots" style={{backgroundColor: '#405865'}}></button>
-      <button id="swatch7" className="pots" style={{backgroundColor: '#8EB5B5'}}></button>
-      <button id="swatch8" className="pots" style={{backgroundColor: '#437048'}}></button>
-      <button id="swatch9" className="pots" style={{backgroundColor: '#60965E'}}></button>
+      <div className="faces">
+        <img src={require('../images/stickers/faces/happy.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/anxious.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/angry.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/joyful.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/sad.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/surprised.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/loving.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/afraid.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/unhappy.svg')} alt="happy face" />
+      </div>
     </div>
+
+    {/* <div id="grid-row">
+      <div className="faces">
+        <p className="sticker-titles">Emotion Icons</p> 
+        <img src={require('../images/stickers/faces/happy.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/anxious.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/angry.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/joyful.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/sad.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/surprised.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/loving.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/afraid.svg')} alt="happy face" />
+        <img src={require('../images/stickers/faces/unhappy.svg')} alt="happy face" />
+      </div>
+    </div> */}
+
   </div>
 )
 
